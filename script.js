7d2b76cfb140d45a3eb5211ed0a87189fe195240
alex jo bobby <alexjocoding@gmail.com>
2025-07-30 22:16:56 +0530

    diff method

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 let startTime = null;
 let charCount = 0;
-const box = document.getElementById('typingBox');
-box.addEventListener('input', () => {
-  if (!startTime) 
-    startTime = Date.now();
-  charCount++;
 
-  const elapsed = (Date.now() - startTime) / 1000;
-  const cps = charCount / elapsed;
-  console.log(cps);
+document.addEventListener('input', (e) => {
+    if (!e.target || !e.target.tagName) return;
 
-  if (charCount % 15 === 0) {
-    checkSpeed(cps);
-  }
-  function checkSpeed(cps) 
-  {
+    const tag = e.target.tagName.toLowerCase();
+    if (tag !== 'input' && tag !== 'textarea') return;
 
-    let file = '';
-    
+    if (!startTime) startTime = Date.now();
 
-    if (cps < 2) file = 'sounds/slow.mp3';
-    else if (cps < 4) file = 'sounds/medium.mp3';
-    else file = 'sounds/fast.mp3';
+    charCount++;
 
-    const audio = new Audio(file);
-    audio.play();
-}
+    const elapsed = (Date.now() - startTime) / 1000; 
+    const cps = charCount / elapsed; 
+
+    if (charCount % 15 === 0) {
+        let file = '';
+
+        if (cps < 2) file = 'audio/slow.mp3';
+        else if (cps < 4) file = 'audio/medium.mp3';
+        else file = 'audio/fast.mp3';
+
+        const audio = new Audio(chrome.runtime.getURL(file));
+        audio.play();
+    }
 });
