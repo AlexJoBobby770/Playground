82d64f72e4a3b23b29a9e54ffce6da2bf1ff7e37
alexjo <alexjocoding@gmail.com>
2025-09-05 19:54:13 +0530

    backend

--- server.js
+++ server.js
@@ -11,3 +11,32 @@ mongoose.connect("mongodb://localhost:27017/mydatabase", {
     useUnifiedTopology: true,
 }).then(() => {console.log("Connected to MongoDB")})
 .catch((err) => {console.log(err)});
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    userid: String,
+});
+ const User=mongoose.model("user", userSchema);
+
+ app.get("/", (req, res) => {res.send("Hello World")});
+  
+ app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username, password });
+    
+    if (user) {
+      res.json({ status: "success" });
+    } else {
+      res.json({ status: "error", message: "Invalid credentials" });
+    }
+  });
+
+  app.post("/signup", async (req, res) => {
+    const { username, password } = req.body;
+    const newUser = new User({ username, password });
+    await newUser.save();
+    res.json({ status: "success" });
+  });
+
+  app.listen(5000, () => console.log("🚀 Server running on http://localhost:5000"));
+
