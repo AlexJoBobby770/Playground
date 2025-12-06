1269e0ae070db826addb787b85a4f77ba50fe071
alexjo <alexjocoding@gmail.com>
2025-12-06 23:00:24 +0530

    .

--- myapi.py
+++ myapi.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI,Path
+from typing import Optional
+from pydantic import BaseModel
 
 app=FastAPI()
 student={
@@ -9,10 +11,44 @@ student={
     }
 }
 
+class Student(BaseModel):
+    name:str
+    age:int
+    sex:str
+
+class UpdateStudent(BaseModel):
+    name:Optional[str]
+    age:Optional[int]
+    sex:Optional[str]
+
+
 @app.get("/")
 def index():
     return {"name":"first data"}
 
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int= Path ( description="id of student u want to see")):
+def get_student(student_id: int= Path ( description="id of student u want to see",gt=0,lt=5)):
     return student[student_id]
+
+@app.get("/by-name")
+def get_by_name(*,name:Optional[str]=None,test:int):
+    for student_id in student:
+        if student[student_id]["name"] == name:
+            return student[student_id]
+    return {"data":"not found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id:int,new_student:Student):
+    if student_id in student:
+        return {"data":"exists"}
+    student[student_id]=new_student
+    return student[student_id]
+
+@app.put("/update_student/{student_id}")
+def update(student_id:int ,student :UpdateStudent):
+    if student_id not in student:
+        return {"error": "student does not exists"}
+    
+    student[student_id]=UpdateStudent
+    return student[student_id]
+    
