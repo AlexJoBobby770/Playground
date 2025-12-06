e1f36950a3e370df54a4e96f7d06d49d85f1cfce
alexjo <alexjocoding@gmail.com>
2025-12-06 18:38:39 +0530

    .

--- myapi.py
+++ myapi.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 
 app=FastAPI()
+student={
+    1:{
+        "name":"alex",
+        "age":20,
+        "sex":"m"
+    }
+}
 
 @app.get("/")
 def index():
     return {"name":"first data"}
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int= Path ( description="id of student u want to see")):
+    return student[student_id]
