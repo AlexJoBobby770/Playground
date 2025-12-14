83a15da947c8a11595fc18c2f5f509dd07303cc7
alexjo <alexjocoding@gmail.com>
2025-12-14 22:52:34 +0530

    .

--- learn/main.py
+++ learn/main.py
@@ -1,10 +1,11 @@
 avg=0
 with open ('learn/text.txt','r') as f:
-    for line in f:
-        f.seek(16)
-        content=f.read()
-        avg+=content
+    for content in f:
+     
+        parts=content.strip().split(',')
+        print(content)
+        avg+=int(parts[2])
 
         
-        print(content)
-print(avg)
+        
+print(f'avg={avg/3}')
